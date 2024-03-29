500c11edd0760923c8d67448bbb8c1d3c8571e74
Kamsi-yonna <etiobikamsi@gmail.com>
2024-03-29 14:34:12 +0100

    feat - optimizing images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,13 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-  schema: z.object({
-    author: z.string(),
-    title: z.string(),
-    date: z.string(),
-    image: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      title: z.string(),
+      date: z.string(),
+      image: image(),
+    }),
 });
 
 export const collections = {
